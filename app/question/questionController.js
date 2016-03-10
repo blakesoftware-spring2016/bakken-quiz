abb8580119c772bbdb6c495a697fb825345714b3
Samira Champlin <smchamplin16@blakeschool.org>
2016-03-10 10:27:48 -0600

    Added back and next routes to question page buttons

--- app/question/questionController.js
+++ app/question/questionController.js
@@ -1,9 +1,15 @@
-app.controller("questionController", ['$scope','$location', function($scope, $location) {
+app.controller("questionController", ['$scope','$location', 'quizData', '$routeParams', function($scope, $location, quizData, $routeParams) {
     $scope.next = function() {
-        $location.path('/results');
+        if($routeParams == quizData.questions[quizData.questions.length - 1]) {
+            $location.path('/results');
+        };
     };
     
     $scope.back = function() {
-        $location.path('/quizDescriptions');
+        if($routeParams == quizData.questions[0]) {
+            $location.path('/quizDescriptions');
+        } else {
+            $location.path('?')
+        }
     };
 }]);
