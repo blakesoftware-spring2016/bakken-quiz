5c310d932d98648689600e8b5d0573a26b5cd455
Samira Champlin <smchamplin16@blakeschool.org>
2016-03-10 08:51:17 -0600

    ng-click functions on question page

--- app/question/questionController.js
+++ app/question/questionController.js
@@ -1,4 +1,9 @@
 app.controller("questionController", ['$scope','$location', function($scope, $location) {
-
+    $scope.next = function() {
+        $location.path('/results');
+    };
     
+    $scope.back = function() {
+        $location.path('/quizDescriptions');
+    };
 }]);
