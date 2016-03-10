75acb910fc3173694e6a4ec343eb90b7493ce269
dajing17 <dajing17@blakeschool.org>
2016-03-09 20:37:40 -0600

    Scripted Index.html

---
+++ app/app.js
@@ -0,0 +1,44 @@
+var app = angular.module("quizApp", ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {  
+	
+	$routeProvider.when('/touchToBegin', {                                          
+		templateUrl: 'touchToBegin/touchToBeginTemplate.html',        
+		controller: "touchToBeginController"
+	});
+	
+	$routeProvider.when('/shareResults', {                                          
+		templateUrl: 'shareResults/shareResultsTemplate.html',        
+		controller: "shareResultsController"
+	});
+	
+	$routeProvider.when('/results', {                                          
+		templateUrl: 'results/resultsTemplate.html',        
+		controller: "resultsController"
+	});
+	
+	$routeProvider.when('/question', {                                          
+		templateUrl: 'question/questionTemplate.html',        
+		controller: "questionController"
+	});
+	
+	$routeProvider.when('/privacy', {                                          
+		templateUrl: 'privacy/privacyTemplate.html',        
+		controller: "privacyController"
+	});
+	
+	$routeProvider.when('/chooseQuiz', {                                          
+		templateUrl: 'chooseQuiz/chooseQuizTemplate.html',        
+		controller: "chooseQuizController"
+	});
+	
+	
+	$routeProvider.when('/other-page', {          
+		templateUrl: 'templates/other-page-template.html'  
+	});
+	
+	$routeProvider.otherwise({
+		redirectTo: '/'
+	});
+	
+}]);
