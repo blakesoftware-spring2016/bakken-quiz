7971f29847a948541cf2e55efd1d81cb463d6622
Samira Champlin <smchamplin16@blakeschool.org>
2016-03-10 19:33:09 -0600

    Back and next variable question paths

--- app/question/questionController.js
+++ app/question/questionController.js
@@ -2,6 +2,9 @@ app.controller("questionController", ['$scope','$location', 'quizData', '$routeP
     $scope.next = function() {
         if($routeParams == quizData.questions[quizData.questions.length - 1]) {
             $location.path('/results');
+        } else {
+            $location.path('/question/' + (Number($routeParams.question-num) + 1));
+            console.log(Number($routeParams.question-num) + 1);
         };
     };
     
@@ -9,7 +12,8 @@ app.controller("questionController", ['$scope','$location', 'quizData', '$routeP
         if($routeParams == quizData.questions[0]) {
             $location.path('/quizDescriptions');
         } else {
-            $location.path('?')
-        }
+            $location.path('/question/' + (Number($routeParams.question-num) - 1));
+            console.log(Number($routeParams.question-num) - 1);
+        };
     };
 }]);
