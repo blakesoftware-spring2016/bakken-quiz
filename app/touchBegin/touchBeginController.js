d0d9ea7776e4e9a865f07124dedb87e1b049bcbb
mthofstadte17 <mthofstadte17@TBSLTMBA872.local>
2016-03-11 00:14:02 -0600

    fixed broken links and added css

--- app/touchBegin/touchBeginController.js
+++ app/touchBegin/touchBeginController.js
@@ -1,6 +1,6 @@
 app.controller("touchBeginController", ['$scope','$location', function($scope, $location) {
   $scope.buttonClick = function() {
-      $location.path('/question');
+      $location.path('/chooseQuiz');
     };
     
 }]);
