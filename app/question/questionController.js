8bd1e3a992dbacef8d37b303c656248d9886a592
Kincaid O'Neil <kincaidoneil@gmail.com>
2016-03-10 23:10:40 -0600

    Fix errors in question controller

--- app/question/questionController.js
+++ app/question/questionController.js
@@ -1,32 +1,33 @@
 app.controller("questionController", ['$scope','$location', '$routeParams', '$http', function($scope, $location, $routeParams, $http) {
 	
 	$http.get('app/questions.json').then(function(response) {
-		$scope.data = response.data;
+		
+		$scope.questions = response.data.questions;
+		
+		$scope.questionNum = Number($routeParams.questionNum);
+	    $scope.question = $scope.questions[$scope.questionNum];
+	    $scope.questionTitle = $scope.question.question;
+		
+	    // Array containing answer objects with "answer": question text, "buckets": []
+	    $scope.questionAnswers = $scope.question.multiChoiceAnswers;
+		
 	});
 	
 	$scope.next = function() {
 		// As always, Angular's syntax is terrible...but it kinda works!
-		if ($routeParams.questionNum == $scope.data.questions.length - 1) {
+		if ($routeParams.questionNum == $scope.questions.length - 1) {
             $location.path('/results');
         } else {
-            $location.path('/question/' + (Number($routeParams.questionNum) + 1));
-            console.log(Number($routeParams.questionNum) + 1);
+            $location.path('/question/' + String(Number($routeParams.questionNum) + 1));
         };
     };
 	
     $scope.back = function() {
-        if($routeParams == quizData.questions[0]) {
+        if ($routeParams == 0) {
             $location.path('/quizDescriptions');
         } else {
-            $location.path('/question/' + (Number($routeParams.questionNum) - 1));
-            console.log(Number($routeParams.questionNum) - 1);
+            $location.path('/question/' + String(Number($routeParams.questionNum) - 1));
         };
     };
-
-    $scope.questionNumber = Number($routeParams.questionNum);
-    $scope.questionData = quizData.questions[questionNumber];
-    $scope.questionTitle = questionData.question;
-
-    //Array containing answer objects with "answer": question text, "buckets": []
-    $scope.questionAnswers = questionData.multiChoiceAnswers;
+	
 }]);
