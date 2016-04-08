d56992db65b1c442a14d3bae9ccfbc9dacc41508
Kincaid O'Neil <kincaidoneil@gmail.com>
2016-04-08 00:58:37 -0500

    Modify question page for multiple quizzes
    Also, clean up some things

--- app/controllers.js
+++ app/controllers.js
@@ -3,34 +3,28 @@ app.controller("chooseQuizController", ['$scope','$location', 'quizData', functi
     
 }]);
 
-
 //privacyController
 app.controller("privacyController", ['$scope','$location', 'quizData', function($scope, $location, quizData) {
-
-
+	
 }]);
-
 
 //questionController
 app.controller("questionController", ['$scope','$location', '$routeParams', 'quizData', function($scope, $location, $routeParams, quizData) {
 	
 	quizData.then(function(response) {
-		
 		// Get parameters from route
-		var quizID = $routeParams.quizID;
-		var questionID = $routeParams.questionID;
-		
+		$scope.quizID = $routeParams.quizID;
+		$scope.questionID = $routeParams.questionID;
 		// Make all data associated with the current quiz available to the template
-		$scope.quiz = response.data[quizID];
+		$scope.quiz = response.data[$scope.quizID];
 		$scope.questions = $scope.quiz.questions;
 		$scope.question = $scope.questions[$scope.questionID];
 		$scope.answers = $scope.question.answers;
-		
 	});
 	
 	$scope.next = function() {
 		// As always, Angular's syntax is terrible...but it kinda works!
-		if ($routeParams.questionNum == $scope.questions.length - 1) {
+		if ($scope.questionID == $scope.questions.length - 1) {
             $location.path('/results');
         } else {
             $location.path('/question/' + String($scope.quizID) + '/' + String(++$scope.questionID));
@@ -38,15 +32,15 @@ app.controller("questionController", ['$scope','$location', '$routeParams', 'qui
     };
 	
     $scope.back = function() {
-        if ($scope.questionNum == 0) {
-            $location.path('/quizDescription');
+        if ($scope.questionID == 0) {
+            $location.path('/quizDescription/' + String($scope.quizID));
         } else {
 			$location.path('/question/' + String($scope.quizID) + '/' + String(--$scope.questionID));
         };
     };
 	
 	$scope.isSelected = function($index) {
-		if ($scope.question.selected == $index) {
+		if ($scope.question.selected === $index) {
 			return true;
 		} else {
 			return false;
@@ -59,53 +53,42 @@ app.controller("questionController", ['$scope','$location', '$routeParams', 'qui
 	
 }]);
 
-
 //quitPageController
 app.controller("quitPageController", ['$scope','$location', 'quizData', function($scope, $location, quizData) {
     
-	
-	
 }]);
-
 
 //quizDescriptionsController
 app.controller("quizDescriptionController", ['$scope','$location','$routeParams', 'quizData', function($scope, $location, $routeParams, quizData) {
     
     quizData.then(function(response) {
-        
         var quizID = $routeParams.quizID;
         $scope.data = response.data[quizID];
-		
 		$scope.question_href = '#/question/' + quizID + '/0';
     });
 	
 }]);
 
-
 //resultsController
 app.controller("resultsController", ['$scope','$location', 'quizData', function($scope, $location, quizData) {
-
-
+	
 }]);
-
 
 //sentConfirmationController
 app.controller("sentConfirmationController", ['$scope','$location', 'quizData', function($scope, $location, quizData) {
-
-
+	
 }]);
-
 
 //shareResultsController
 app.controller("shareResultsController", ['$scope','$location', 'quizData', function($scope, $location, quizData) {
-
-
+	
 }]);
-
 
 //touchBeginController
 app.controller('touchBeginController', ['$scope','$location', function($scope, $location) {
+	
 	$scope.buttonClick = function() {
 		$location.path('/chooseQuiz');
 	};
+	
 }]);
