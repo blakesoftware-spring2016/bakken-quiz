d65da81a5a524e3d3fd76c1d340ae03ff7488328
dajing17 <dajing17@blakeschool.org>
2016-04-26 11:42:52 -0500

    Share Results Popup

--- app/controllers/shareResultsController.js
+++ app/controllers/shareResultsController.js
@@ -34,31 +34,16 @@ app.controller("shareResultsController", ['$scope','$location', 'quizData','$uib
 
 	};
 
-    $scope.open = function() {
-
-        var modalInstance = $uibModal.open({
-            animation: true,
-            templateUrl: 'app/templates/popupContent.html',
-            controller: ['$scope', '$uibModalInstance', function($scope, $uibModalInstance) {
-				$scope.popupTitle = "Are you sure you want to quit?";
-				$scope.confirm = "Yes";
-				$scope.back = "No";
-
-				$scope.dismiss = function(value) {
-        			$uibModalInstance.close(value);
-				};
-			}],
-            size: 'lg'
-        });
-
-        modalInstance.result.then(function(dismissVal) {
-            if(dismissVal === "Yes") {
-				for(var property in session_answers) {
-					delete session_answers[property];
-				};
-			};
-        });
-
-    };
-
+      $scope.back = function() {
+		for(var property in session_answers) {
+			delete session_answers[property];
+		};
+		$location.path('/chooseQuiz');
+	}
+      
+      $scope.continue = function() {
+		$location.path('/results');
+	}
+      
+      
 }]);
