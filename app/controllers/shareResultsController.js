4d63daad35f5d1ce40abb73b917ee5487e38c48f
Kincaid O'Neil <kincaidoneil@gmail.com>
2016-05-14 15:54:31 -0500

    Remove name fields from share results

--- app/controllers/shareResultsController.js
+++ app/controllers/shareResultsController.js
@@ -13,16 +13,9 @@ app.controller('shareResultsController', function($scope, $location, quizData, $
 			// Send a POST request to the server to send the email
 			var data = {
 				email: $scope.email,
-				email2: $scope.emailCheck,
-				firstName: $scope.firstName,
-				lastName: $scope.lastName,
 				title: session_results.title,
 				description: session_results.description
 			};
-			// Only send a quote if it's not undefined or empty
-			if (session_results.quote) {
-				data.quote = session_results.quote;
-			}
 			$http.post('/shareResults', data).then(function(response) {
 				console.log(response);
 			}, function(error) {
