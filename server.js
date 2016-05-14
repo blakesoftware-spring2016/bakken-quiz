10716665ecc49b4c65bbc9a10ca55b693bf95c5b
Kincaid O'Neil <kincaidoneil@gmail.com>
2016-05-14 15:55:09 -0500

    Add updated email template to server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // Setup the Express server
 const express = require('express')
 const body_parser = require('body-parser')
-const nodemailer = require('nodemailer')
+const node_mailer = require('nodemailer')
 
 const app = express()
 
@@ -15,23 +15,36 @@ app.use(express.static('./'))
 
 // Add endpoint to send the share results email
 app.post('/shareResults', (req, res) => {
-	// Verify all the necessary data was sent and emails match
-	if (!req.body.email || !req.body.email2 || !req.body.firstName || !req.body.lastName || !req.body.title || !req.body.description || req.body.email !== req.body.email2) {
+	// Verify all the necessary data was sent
+	if (!req.body.email || !req.body.title || !req.body.description) {
 		res.status(400).send()
 		return
 	}
 	// Create reusable transporter object using the default SMTP transport
-	let transporter = nodemailer.createTransport('smtps://maryshelley.bakken%40gmail.com:hello123world@smtp.gmail.com')
+	let transporter = node_mailer.createTransport('smtps://maryshelley.bakken%40gmail.com:hello123world@smtp.gmail.com')
 	// Setup e-mail data with unicode symbols
-	let html = req.body.description
-	let text = req.body.description
-	if (req.body.quote) {
-		html += '<br /><br /><i>' + req.body.quote + '</i>'
-		text += '\n\n' + req.body.quote
-	}
+	let html = `${req.body.description}
+		<br /><br />
+		Thanks for visiting the Mary and Her Monster exhibit at The Bakken and taking the quiz!
+		<br /><br />
+		<a href="https://www.facebook.com/thebakkenmuseum">Like us on Facebook</a> | <a href="https://twitter.com/thebakken">Follow us on Twitter</a> | <a href="https://www.instagram.com/thebakkenmuseum">Follow us on Instagram</a>
+		<br /><br />
+		Register to receive more information about The Bakken and its programs <a href="https://thebakken.org/Newsletter-Sign-Up">here</a>.
+		<br /><br />
+		Email <a href="mailto:info@thebakken.org">info@thebakken.org</a> to learn more about The Bakken.
+		<br /><br />
+		<a href="#">The Bakken’s Privacy Policy</a>`
+	let text = req.body.description +
+		`Thanks for visiting the Mary and Her Monster exhibit at The Bakken and taking the quiz!\n\n` +
+		`Like us on Facebook: https://www.facebook.com/thebakkenmuseum\n` +
+		`Follow us on Twitter: https://twitter.com/thebakken\n` +
+		`Follow us on Instagram: https://www.instagram.com/thebakkenmuseum\n\n` +
+		`Register to receive more information about The Bakken and its programs: https://thebakken.org/Newsletter-Sign-Up\n\n` +
+		`Email info@thebakken.org to learn more about The Bakken.\n\n` +
+		`The Bakken’s Privacy Policy: link_goes_here`
 	let options = {
 		from: '"The Bakken Museum" <maryshelley.bakken@gmail.com>',
-		to: '"' + req.body.firstName + ' ' + req.body.lastName + '" <' + req.body.email + '>',
+		to: req.body.email,
 		subject: req.body.title,
 		text, html
 	}
