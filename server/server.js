ebb1c809f15a6c82b08e289cd293a367a5dd52b0
Kincaid O'Neil <kincaidoneil@gmail.com>
2016-05-23 12:54:12 -0500

    Fix error with email fallback script
    Also, ES6-ify Sean's preloading code (i.e. very unnecessarily remove
    semicolons)

--- server/server.js
+++ server/server.js
@@ -21,21 +21,24 @@ app.post('/shareResults', (req, res) => {
 	// Verify all the necessary data was sent
 	if (!req.body.email || !req.body.title || !req.body.description) {
 		res.status(400).send()
-		throw 'Necessary parameters were not sent.'
+		console.log('Necessary parameters were not sent.')
+		return
 	}
 	// Read the server configuration
 	fs.readFile(__dirname + '/config.json', (error, data) => {
 		// Send a bad request response if there was an error
 		if (error) {
 			res.status(400).send()
-			throw error
+			console.log(error)
+			return
 		}
 		let configs = JSON.parse(data)
 		fs.readFile(__dirname + '/template.html', (error, data) => {
 			// Send a bad request response if there was an error
 			if (error) {
 				res.status(400).send()
-				throw error
+				console.log(error)
+				return
 			}
 			// Setup the email options and template
 			let template = data.toString()
@@ -55,14 +58,15 @@ app.post('/shareResults', (req, res) => {
 					if (error) {
 						// If the current config failed and is the last config to try...
 						if (config_index === configs.length - 1) {
-							// Respond with an error
+							// Respond to request with an error
 							res.status(400).send()
-							throw error
+							console.log(error)
+							return
 						}
 						// If there are additional configs to try, log error and try again
 						else {
 							console.log(error)
-							sendEmail(config_index++)
+							sendEmail(++config_index)
 							return
 						}
 					}
@@ -78,36 +82,26 @@ app.post('/shareResults', (req, res) => {
 })
 
 app.get('/preloadImages', (req, res) => {
-
-    var imageUrls = imagesInDirectory('../assets/images');
+    let imageUrls = imagesInDirectory('../assets/images');
     imageUrls = imageUrls.map(function(url) {
     	return url.slice(3); // Remove the ../
-    });
-
-    res.send(imageUrls);
-});
+    })
+    res.send(imageUrls)
+})
 
 function imagesInDirectory(dir) {
-
-    var results = [];
-
+    let results = []
     fs.readdirSync(dir).forEach(function(file) {
-
-        file = dir + '/' + file;
-        var stat = fs.statSync(file);
-
+        file = dir + '/' + file
+        var stat = fs.statSync(file)
         if (stat && stat.isDirectory()) {
-            results = results.concat(imagesInDirectory(file));
-        } 
-        else {
-        	if (file.endsWith('.png') || file.endsWith('.jpg') || file.endsWith('.jpeg')) {
-        		results.push(file);
-        	}
+            results = results.concat(imagesInDirectory(file))
         }
-
-    });
-
-    return results;
+        else if (file.endsWith('.png') || file.endsWith('.jpg') || file.endsWith('.jpeg')) {
+			results.push(file)
+        }
+    })
+    return results
 }
 
 // Handle 404 errors
